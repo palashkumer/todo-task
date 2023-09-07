f1859859fe66b3d2c4049b49c9e8919e0c1a2393
unknown <pkpalash237.com>
2023-09-07 10:36:25 +0600

    fix: frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,91 +1,3 @@
-// import './App.css'
-// import {  Routes, Route,Link } from 'react-router-dom'
-// import TaskList from './components/TaskList'
-// import AddTask from './components/AddTask'
-// // import EditTask from './components/EditTask'
-
-
-// function App() {
-
-//   return (
-//     <>
-//     <div className="container">
-//     <div className="App">
-//       <h2 className="mt-4  text-center">Task List</h2>
-//       <Link to="/addtask" className="btn btn-success mx-3 mt-4">AddTask</Link>
-//       <Routes>
-     
-//       <Route path="/tasklist" element= { <TaskList/> } />
-//       {/* <Route path='/' element= { <TaskList/> } /> */}
-//       <Route path="/addtask" element= { <AddTask/> } />
-//       {/* <Route path="/edittask/:id" element= { <EditTask/> } /> */}
-//       </Routes>
-//  </div>
-//  </div>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-// import  { useState } from "react";
-// import { Routes, Route } from "react-router-dom";
-// import TaskList from "./components/TaskList";
-// import EditTask from "./components/EditTask";
-// import AddTask from "./components/AddTask";
-
-// function App() {
-//   const [showAddTaskModal, setShowAddTaskModal] = useState(false);
-
-//   const toggleAddTaskModal = () => {
-//     setShowAddTaskModal(!showAddTaskModal);
-//   };
-
-//   return (
-//     <>
-//       <div className="container">
-//         <div className="App">
-//           <h2 className="mt-4 text-center">Task List</h2>
-//           {!showAddTaskModal && ( // Render the button only if the modal is closed
-//             <button
-//               className="btn btn-success mx-3 mt-4"
-//               onClick={toggleAddTaskModal}
-//             >
-//               Add Task
-//             </button>
-//           )}
-//           <Routes>
-            
-//             <Route
-//               path="/addTask"
-//               element={
-//                 <>
-//                   <TaskList />
-//                   {showAddTaskModal && (
-//                     <AddTask
-//                       showAddTaskModal={showAddTaskModal}
-//                       toggleAddTaskModal={toggleAddTaskModal}
-//                     />
-//                   )}
-//                 </>
-//               }
-//             />
-//             <Route path="/Tasklist" element={<TaskList />} />
-//             <Route path="/editTask/:id" element={<EditTask />} />
-
-//           </Routes>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
 
 import  { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
@@ -112,7 +24,6 @@ function App() {
         </Routes>
       </div>
 
-      {/* Add Task Modal */}
       <div
         className={`modal ${showAddTaskModal ? 'show' : ''}`}
         tabIndex="-1"
